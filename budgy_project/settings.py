8085b8282c22a1a9217ba245e40cdd7170b75de4
Amina0426 <amublue26@gmail.com>
2026-05-10 13:34:54 +0530

    production ready2

--- budgy_project/settings.py
+++ budgy_project/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","https://budgy-q39z.onrender.com"]
 
 
 # Application definition
