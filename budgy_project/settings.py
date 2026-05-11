0c2db4ed6a40899d7c54e0384b6b8b53c9b3485f
Amina0426 <amublue26@gmail.com>
2026-05-11 13:22:08 +0530

    check for keepLoggedIn

--- budgy_project/settings.py
+++ budgy_project/settings.py
@@ -116,7 +116,12 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_COOKIE_SECURE = True
+
+SESSION_COOKIE_SAMESITE = "Lax"
+
 CSRF_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = "Lax"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
