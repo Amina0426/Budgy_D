92cff14436aec049023f8495cd702bf58fd3dd9b
Amina0426 <amublue26@gmail.com>
2026-05-13 12:13:19 +0530

    v1.0.0 commit

--- budgy_app/static/service-worker.js
+++ budgy_app/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "expense-cache-v1.0.0";
+const CACHE_NAME = "budgy-cache-v1.0.0";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
--- budgy_project/settings.py
+++ budgy_project/settings.py
@@ -88,13 +88,18 @@ WSGI_APPLICATION = 'budgy_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
-}
 
-
+if os.getenv("RENDER"):
+    DATABASES = {
+        "default": dj_database_url.config()
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
